85e62632804e28f12388a507e8b56ee9440a04b2
kiran <kirankumar215@gmail.com>
2019-03-25 20:37:10 -0400

    SQL server changes

--- src/com/client/mmapi/dao/MMDao.java
+++ src/com/client/mmapi/dao/MMDao.java
@@ -1,6 +1,8 @@
 package com.client.mmapi.dao;
 import java.sql.*;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
 public class MMDao {
 	
 	Connection con;
@@ -9,8 +11,31 @@ public class MMDao {
 	public MMDao(){	
 		try {
 			//1. Get a connection to database
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmapi_schema","mmapi_admin", "MmapiAdmin!408");
+			//con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmapi_schema","mmapi_admin", "MmapiAdmin!408");
+			//con = DriverManager.getConnection("jdbc:sqlserver://localhost:49688/mmapi_schema","sa", "2305");
+			/*
+			String dbURL = "jdbc:sqlserver://localhost\\sqlexpress/dbo";
+			String user = "sa";
+			String pass = "2305";
+			con = DriverManager.getConnection(dbURL, user, pass);*/
+			
+			 
+	            String serverName = "localhost";
+	            String portNumber = "49688";
+	            String databaseName = "UXI";
+	            String username = "sa";
+	            String password = "2305";
 
+	            // Establish the connection.
+	            SQLServerDataSource ds = new SQLServerDataSource();
+	            ds.setServerName(serverName);
+	            ds.setPortNumber(Integer.parseInt(portNumber));
+	            ds.setDatabaseName(databaseName);
+	            ds.setUser(username);
+	            ds.setPassword(password);
+			
+	            con = ds.getConnection();
+	            
 		}
 		catch(Exception exc) {
 			exc.printStackTrace();
